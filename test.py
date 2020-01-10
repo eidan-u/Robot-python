aa251859379f04cfa2c671b520532f2b070e2c94
Your Name <you@example.com>
2020-01-10 17:48:10 +0100

    tout fini

--- test.py
+++ test.py
@@ -1,30 +1,49 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO #on importe le module GPIO pour controler les ports
+import time #on importe le module time ce qui permettra d'utiliser des fonction comme time.sleep()
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)#on explique sur quelle numérotation de port on se trouve(BOARD ou BCM ) ici en BCM chaque ports possedent un numero unique (non ordonné ni croissant)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+#IMPORTANT ici on se trouve dans le cas ou l'on possede 1 capteur donc une entrée un moteur connecté à une carte de controle de sens à 2 sorties (voir explications ci dessous).
+
+GPIO.setup(7, GPIO.IN) #on initialise le port utilisé dans ce cas si c'est le 7 pour le capteur. Il y est rentré la valeur in car le capteur envoie vers le micro processeur c'est une entrée.
+GPIO.setup(17, GPIO.OUT)#ici on fait pareil, ce port serivra au moteur pour un send et il est out car les info viennent du microprocesseur vers le moteur
+GPIO.setup(18, GPIO.OUT)# pareil mais pour l'autre sens
+GPIO.setwarnings(False) #supprime les messages d'erreures
+print ("Demarrage du capteur")
+time.sleep(2) #la fonction sleep() permet de creer un delay avant la suite du code soit 2 secondes ici
+print ("Capteur pret a detecte un mouvement")
+while True:
+   if GPIO.input(7):
+      print("arret des roues")
+      GPIO.output(17, False) #on arrete les roues
+      GPIO.output(18, False)#on arrete les roues
+      time.sleep(2)
+   time.sleep(0.1)
+
+else:
+    print('héééééé zé party')
+    while True:
+        try:
+            GPIO.output(17, True)#permet de faire tourner dans un sens
+            GPIO.output(18, False)
+            time.sleep(2)
+            print("dans l'autre sens")
+            GPIO.output(17, False)#permet de faire tourner dans l'autre sens
+            GPIO.output(18, True)
+            time.sleep(2)
+        except(KeyboardInterrupt): #ici cette commande permet de faire une execption au prgramme seulement si "controle-c" est pressé
+            Print('on arrete tout')
+            GPIO.output(17, False)
+            GPIO.output(18, False)
+            break #Stop le while True
+            GPIO.cleanup() #permet de re-initialiser les ports
+
+
+
+
+
+
+
+
+
+
